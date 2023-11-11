6d8f5f069748df9f49f2e301fcd598f9f54b6131
Lucie Dušková <lucie.duskova87@gmail.com>
2023-11-11 12:49:19 +0100

    3látky

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,15 @@
-import { czechitasRenderVitePlugin } from '@czechitas/render/plugin';
-import { glob } from 'glob';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'src/pages',
-  publicDir: '../../public',
+  root: './src',
+  publicDir: '../public',
   server: {
     hmr: false,
   },
   build: {
-    outDir: '../../dist',
-    emptyOutDir: true,
-    target: 'es2022',
-    rollupOptions: {
-      input: glob.sync('src/pages/**/*.html'),
-    },
+    outDir: '../dist',
     modulePreload: false,
   },
-  plugins: [czechitasRenderVitePlugin()],
+  plugins: [react()],
 });
