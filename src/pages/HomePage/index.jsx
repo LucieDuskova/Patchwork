7615b4bf850ab7febbf043917642bfdd516c24a0
Lucie Dušková <lucie.duskova87@gmail.com>
2023-12-01 17:56:22 +0100

    text do HomePage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,8 +3,40 @@ import './style.css';
 export const HomePage = () => {
   return (
     <main>
-      <h2>Proč hra??</h2>
-      <p>Děláme Patchwork, protože... </p>
+      <h2>
+        Vítejte ve světě Patchwork, kde se setkává tvůrčí umění s taktikou a kde každý hráč se stává mistrem vlastního designu. Patchwork není jen hra, je to cesta do světa barev, vzorů a nekonečných možností.
+      </h2>
+
+      <p>
+        <h2>1. Stvořte Svůj Unikátní Svět: </h2>
+        V Patchwork neexistují žádná omezení pro vaši kreativitu. Z každého kousku látky vytvořte jedinečný obrazec, který bude zrcadlit vaše umělecké vlohy. Z každé partie vyjdete s originálním dílem, které bude neseno vaším jedinečným stylem.
+      </p>
+
+      <p>
+        <h2>2. Taktické Rozhodování na Každém Kroku: </h2>
+        Každý krok je důležitý. Musíte pečlivě vybírat kousky látky a umísťovat je na svůj hrací plán tak, aby vám přinesly co nejvíce bodů. Sledujte svého soupeře, plánujte své tahy a překonávejte výzvy, které vám hra klade.
+      </p>
+
+      <p>
+        <h2>3. Jednoduchá Pravidla, Komplexní Strategie: </h2>
+        Patchwork si zachovává jednoduchá pravidla, což z ní činí přístupnou pro všechny hráče. Ale nechte se zmást - pod povrchem skrývá hloubku strategie, která potěší i ty nejzkušenější hráče. Budete muset kombinovat obratnost s plánováním, abyste byli nejlepší.
+      </p>
+
+      <p>
+        <h2>4. Rodinná Zábava a Soutěživé Souboje:</h2> Patchwork je skvělá volba pro rodinnou zábavu i pro soutěživé souboje s přáteli. Vytvářejte a soutěžte o nejlepší přikrývky, buďte kreativní a užívejte si společnost těch, které máte rádi.
+      </p>
+
+      <p>
+        <h2>5. Rychlá Hra, Nekonečný Zážitek: </h2>
+        když jsou partie krátké, dovolte si ponořit se do nekonečného zážitku.
+        Každá hra je novým dobrodružstvím, kde můžete objevovat nové taktiky,
+        zlepšovat své dovednosti a užívat si radost z vytváření.
+      </p>
+
+      <p>
+        Vstupte do světa Patchwork a staňte se mistrem látky, nití a strategie. Hra, která osloví vaše umělecké i taktické sklony, Patchwork, je připravena na to, aby vás vtáhla do svého kouzelného světa. Připravte se na nekonečnou zábavu a soutěž v jednom z nejlepších deskových her.
+        <strong> Patchwork - kde každý tah tvoří příběh.</strong>
+      </p>
     </main>
   );
 };
