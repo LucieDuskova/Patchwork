5ff59224f1d61bcaa3ed6d535baa3bc26bfbcaa1
Linda Hrdinová <hrlin@seznam.cz>
2023-12-03 18:03:03 +0100

    vykreslování látek na dece hráče - funguje

--- src/pages/GamePage/index.jsx
+++ src/pages/GamePage/index.jsx
@@ -4,7 +4,6 @@ import { Patches } from '../../components/Patches';
 import { useReducer } from 'react';
 import { Player } from '../../components/Player';
 import { reducer, defaultState } from '../../components/Reducer';
-import './style.css';
 
 export const GamePage = () => {
   const [state, dispatch] = useReducer(reducer, defaultState);
@@ -13,21 +12,19 @@ export const GamePage = () => {
       <GameArrayTime state={state} />
       <Patches dispatch={dispatch} state={state} />
 
-      <div className="GamePage__playerBoards">
-        <Player
-          playerNumber={1}
-          player={state.player1}
-          isCurrentPlayer={state.currentPlayer === 'player1'}
-          playerFieldSize={state.playerFieldSize}
-        />
-        <GameMenu dispatch={dispatch} state={state} />
-        <Player
-          playerNumber={2}
-          player={state.player2}
-          isCurrentPlayer={state.currentPlayer === 'player2'}
-          playerFieldSize={state.playerFieldSize}
-        />
-      </div>
+      <Player
+        playerNumber={1}
+        player={state.player1}
+        isCurrentPlayer={state.currentPlayer === 'player1'}
+        playerFieldSize={state.playerFieldSize}
+      />
+      <GameMenu dispatch={dispatch} state={state} />
+      <Player
+        playerNumber={2}
+        player={state.player2}
+        isCurrentPlayer={state.currentPlayer === 'player2'}
+        playerFieldSize={state.playerFieldSize}
+      />
     </>
   );
 };
