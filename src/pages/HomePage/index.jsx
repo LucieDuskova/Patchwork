463697dd64c92aa32821710c8926c80e66496f9e
Linda Hrdinová <hrlin@seznam.cz>
2023-12-08 19:56:04 +0100

    homepage - velká písmena

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -8,34 +8,34 @@ export const HomePage = () => {
         každý hráč se stává mistrem vlastního designu. Patchwork není jen hra,
         je to cesta do světa barev, vzorů a nekonečných možností.
       </h2>
-      <h3>Stvořte Svůj Unikátní Svět</h3>
+      <h3>Stvořte svůj unikátní svět</h3>
       <p>
         V Patchwork neexistují žádná omezení pro vaši kreativitu. Z každého
         kousku látky vytvořte jedinečný obrazec, který bude zrcadlit vaše
         umělecké vlohy. Z každé partie vyjdete s originálním dílem, které bude
         neseno vaším jedinečným stylem.
       </p>
-      <h3>Taktické Rozhodování na Každém Kroku</h3>
+      <h3>Taktické rozhodování na každém kroku</h3>
       <p>
         Každý krok je důležitý. Musíte pečlivě vybírat kousky látky a umísťovat
         je na svůj hrací plán tak, aby vám přinesly co nejvíce bodů. Sledujte
         svého soupeře, plánujte své tahy a překonávejte výzvy, které vám hra
         klade.
       </p>
-      <h3>Jednoduchá Pravidla, Komplexní Strategie</h3>
+      <h3>Jednoduchá pravidla, komplexní strategie</h3>
       <p>
         Patchwork si zachovává jednoduchá pravidla, což z ní činí přístupnou pro
         všechny hráče. Ale nechte se zmást - pod povrchem skrývá hloubku
         strategie, která potěší i ty nejzkušenější hráče. Budete muset
         kombinovat obratnost s plánováním, abyste byli nejlepší.
       </p>
-      <h3>Rodinná Zábava a Soutěživé Souboje</h3>
+      <h3>Rodinná zábava a soutěživé souboje</h3>
       <p>
         Patchwork je skvělá volba pro rodinnou zábavu i pro soutěživé souboje s
         přáteli. Vytvářejte a soutěžte o nejlepší přikrývky, buďte kreativní a
         užívejte si společnost těch, které máte rádi.
       </p>
-      <h3>Rychlá Hra, Nekonečný Zážitek</h3>
+      <h3>Rychlá hra, nekonečný zážitek</h3>
       <p>
         když jsou partie krátké, dovolte si ponořit se do nekonečného zážitku.
         Každá hra je novým dobrodružstvím, kde můžete objevovat nové taktiky,
